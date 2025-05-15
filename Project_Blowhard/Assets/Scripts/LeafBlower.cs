a79ce8d7b580802378480513765c3fd0743b6480
GhostValley <emailgregsue@gmail.com>
2025-05-15 20:12:39 +0100

    Adding Leaf Blowing Physics
    Adding leaf blowing physics to leaf quads.

---
+++ Project_Blowhard/Assets/Scripts/LeafBlower.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class LeafBlower : MonoBehaviour
+{
+
+    public Transform BlowerNozzle;
+    public float windForce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            BlowWind();
+        }
+
+    }
+
+
+    void BlowWind() {
+        float blowRadius = 5f;
+        Vector3 blowDirection = transform.forward;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, blowRadius);
+
+        foreach (var hit in hitColliders) {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null) {
+                Vector3 directionToLeaf = (hit.transform.position - transform.position).normalized;
+                float forceAmount = Mathf.Max(0, Vector3.Dot(directionToLeaf, blowDirection)) * windForce;
+                rb.AddForce(blowDirection * forceAmount, ForceMode.Impulse);
+            }
+        }
+    }
+
+
+
+}
